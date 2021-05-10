e520c796e537727ac471b380d79437b1c21dab36
Bruce Bjorklund <bjorklund.bruce@gmail.com>
2021-05-10 10:13:01 -0700

    WIP

--- client-connect/src/main.rs
+++ client-connect/src/main.rs
@@ -3,7 +3,7 @@ use mini_redis::{client, Result};
 #[tokio::main]
 pub async fn main() -> Result<()> {
     // Open a connection to the mini-redis address.
-    let mut client = client::connect("127.0.0.1:6379").await?;
+    let mut client = client::connect("127.0.0.1:8080").await?;
 
     // Set the key "hello" with value "world"
     client.set("hello", "world".into()).await?;
